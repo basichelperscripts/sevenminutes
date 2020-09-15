108bf106165fd7c3ee7623d8dd9059a938e3be4c
Samuel Garratt <samuel.garratt@gmail.com>
2020-09-16 06:34:17 +1200

    Move increaseStep into countdown.js file

--- js/countdown.js
+++ js/countdown.js
@@ -18,23 +18,51 @@ function getTimeRemaining(endtime) {
     };
 }
 
-function initializeClock(id, endtime) {            
-    var clock = document.getElementById(id);  
+function increaseStep() {
+    let count = parseInt($counter.value);
+    if (count < 6)
+    {
+        $counter.value = parseInt($counter.value) + 1;
+        index++;
+    }
+    else {
+        // Reset counter at limit
+        $counter.value = 0;
+        index = 0;
+    }
+    // refresh values on the screen
+    document.getElementsByTagName("h1")[0].innerHTML = period[index].title;
+    document.getElementById("describeMe").innerHTML = period[index].description;
+
+    // Clear interval
+    clearInterval(currentTimer);
+    // Set new time
+    timeInSeconds = period[index].time;
+
+    let newTime = Date.parse(new Date());
+    let newdeadline = new Date(newTime + timeInSeconds*1000);
+
+    currentTimer = initializeClock('clockdiv', newdeadline);
+}
+
+/// initialize a clock for a given id and endtime
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
     var minutesSpan = clock.querySelector('.minutes');
     var secondsSpan = clock.querySelector('.seconds');
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-    
+
     if (t.total > 0) {
         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
     }
-    else {        
+    else {
             minutesSpan.innerHTML = '00';
             secondsSpan.innerHTML = '00';
         }
-      
+
         if (t.total <= 0) {
             clearInterval(timeinterval);
             ion.sound.play("bell_ring");
